41b45577a466b02dcd11976393dfbe3d14ec28e7
AnegueAlbq <enzo.anegue@germinare.org.br>
2026-08-18 08:43:14 -0300

    fix: adjust project structure to React config

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-const idade: number = 16;
-const nome: string = "Ana";
-const aprovado: boolean = true;
-const notas: number[] = [8, 9, 10];
-const aluno: { nome: string; idade: number } = { nome, idade };
-
-console.log(nome, idade, aprovado, notas, aluno);
